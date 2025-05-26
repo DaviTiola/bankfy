6e0256078961cdcd6557e382642f085a74f9e616
DaviTiola <davitiolafernandes@gmail.com>
2025-05-26 11:40:50 -0300

    initial project setup with Spring Boot

---
+++ src/test/java/dev/davitiola/bankfy/BankfyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.davitiola.bankfy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankfyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
